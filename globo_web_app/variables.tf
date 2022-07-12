813602fa4358a39ec9029ba8d8e588b27552fb80
CyrilBaah <cyril.baah@amalitech.org>
2022-07-12 15:54:49 +0000

    random for versioning

--- globo_web_app/variables.tf
+++ globo_web_app/variables.tf
@@ -1,14 +1,14 @@
-variable "aws_access_key" {
-  type = string
-  description = "AWS Access key"
-  sensitive = true
-}
+# variable "aws_access_key" {
+#   type = string
+#   description = "AWS Access key"
+#   sensitive = true
+# }
 
-variable "aws_secret_key" {
-  type = string
-  description = "AWS Secret key"
-  sensitive = true
-}
+# variable "aws_secret_key" {
+#   type = string
+#   description = "AWS Secret key"
+#   sensitive = true
+# }
 
 variable "aws_region" {
   type = string
